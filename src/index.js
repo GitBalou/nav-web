e7b86f3349638292a5dedfb988ce0a01b6ebe6b8
Gabriel DAUDIN <gabriel.dl20@formation.imie>
2017-04-04 17:17:38 +0200

    starting Router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const {Provider} = ReactRedux;
+//const {Router, Route} = ReactRouter;
+//import { Router, Route, Link } from 'react-router';
+var ReactRouter = require('react-router')
+var Router = ReactRouter.Router
+var Route = ReactRouter.Route
+var Link = ReactRouter.Link
 
 store.subscribe(()=> console.log(store.getState()));
 
 //Component App
 const App = () => (
 	<Provider store={store}>
-  		<NavList />
+		<Router>
+    		<Route path="/" component={NavList} />
+    	</Router>
   	</Provider>
 );
 
